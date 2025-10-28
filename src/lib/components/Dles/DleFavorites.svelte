2ef85e17961fbf25c799eff72ebc59d0149629d2
Joshua Felmeden <josh.felmeden@gmail.com>
2025-10-28 15:25:07 +0000

    Improve export buttons in favorites

--- src/lib/components/Dles/DleFavorites.svelte
+++ src/lib/components/Dles/DleFavorites.svelte
@@ -96,7 +96,7 @@
 {#if !loading}
   <h2 class="my-2 text-2xl text-center underline">Favorites</h2>
 
-  <div class="flex justify-center mb-4">
+  <div class="flex justify-center gap-2 mb-4">
     <button class="btn flex items-center" on:click={openSearchModal}>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 mr-2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
